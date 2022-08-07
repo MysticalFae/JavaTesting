9638bf041f1125f9b7509e0ec6b3d1a1d32082fb
MysticalFae <sky533539@gmail.com>
2022-08-06 21:13:51 -0500

    C to F

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class TempConversion {
+
+    static Scanner userTemp = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        double tempC = 0;
+
+        System.out.print("Enter in a temp in Celcius: ");
+
+        if (userTemp.hasNextDouble()) {
+            tempC = userTemp.nextDouble();
+
+        }
+
+        double tempF = tempC * 1.8 + 32;
+
+        System.out.println();
+        System.out.println("The temp " + tempC + " in Farhiet is " + tempF);
 
     }
 }
